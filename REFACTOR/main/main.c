d17553739414cf221c4b24ad208ef7d54c64d36b
Li Wing <liwramos1@gmail.com>
2023-04-11 19:15:39 -0300

    queue to comm between tasks

--- REFACTOR/main/main.c
+++ REFACTOR/main/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
 #include "soc/soc_caps.h"
 #include "esp_log.h"
 
@@ -12,7 +13,10 @@
 #include "font8x8_basic.h"
 #include <string.h>
 
+#define SAMPLE_TIMES    20
+
 const static char *TAG = "MAIN";
+static QueueHandle_t queue_buff;
 
 /*-- ADC General --*/
 void adc_task(void*p);
@@ -30,6 +34,8 @@ void app_main(void)
     adc_init();
     ssd_init();
 
+    queue_buff = xQueueCreate(5, sizeof(int));
+
     xTaskCreate(ssd_task, "ssd_task", 2048 * 8, NULL, 5, NULL);
     xTaskCreate(adc_task, "adc_task", 2048 * 8, NULL, 10, NULL);
 
@@ -38,9 +44,20 @@ void app_main(void)
 void adc_task(void* p)
 {
     while (1) {
-        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN0, &adc_raw));
-        ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_handle, adc_raw, &voltage));
-        ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, voltage);
+        int sensor_voltage = 0;
+        for(uint8_t i = 0; i < SAMPLE_TIMES; i++){
+            ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN0, &adc_raw));
+            ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_handle, adc_raw, &voltage));
+            sensor_voltage += voltage;
+            vTaskDelay(pdMS_TO_TICKS(10));
+        }
+        sensor_voltage /= SAMPLE_TIMES;
+
+        xQueueSend(queue_buff, (void*)&sensor_voltage, 10);
+
+        ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", 
+            ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, sensor_voltage
+        );
 
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
@@ -67,17 +84,20 @@ void ssd_init(void)
 void ssd_task(void* p)
 {
     char lineChar[400];
+    int print_data = 0;
 
 	// Scroll Down
 	ssd1306_clear_screen(&dev, false);
 	ssd1306_contrast(&dev, 0xff);
 	ssd1306_display_text(&dev, 0, "--METHANE PPM--", 15, true);
-	//ssd1306_software_scroll(&dev, 1, 7);
 	ssd1306_software_scroll(&dev, 1, (dev._pages - 1) );
 	for (int line=0;;line++) {
-		lineChar[0] = 0x02;
-		sprintf(&lineChar[1], "%d: %.6f", line, 0.123);
-		ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
-		vTaskDelay(500 / portTICK_PERIOD_MS);
+        if(xQueueReceive(queue_buff, (void*)&print_data, portMAX_DELAY)){
+            if(line > 9) line = 0;
+            lineChar[0] = 0x02;
+            sprintf(&lineChar[1], "%d: %d", line, print_data);
+            ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
+            vTaskDelay(500 / portTICK_PERIOD_MS);
+        }
 	}
 }
